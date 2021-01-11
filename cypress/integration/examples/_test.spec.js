b34ceb801cfe431e564d7a82aa4c4f60990c696a
scpex <mr.nathakit@live.com>
2021-01-11 17:04:58 +0700

    update test(every pages)

--- cypress/integration/examples/_test.spec.js
+++ cypress/integration/examples/_test.spec.js
@@ -5,7 +5,7 @@ context('Test Movie Demo', () => {
     cy.visit('http://localhost:3000')
   })
 
-  it('click on now playing', () => {
+  it('now playing', () => {
     cy.location().should((localtion) => {
       expect(localtion.pathname).to.eq('/')
     })
@@ -15,9 +15,36 @@ context('Test Movie Demo', () => {
     cy.location().should((localtion) => {
       expect(localtion.pathname).to.eq('/nowplaying')
     })
+
+    cy.scrollTo('bottom', { duration: 2000 })
+
+    cy.get('h1.now-playing-title').contains('Movies In Theaters Now').should('be.visible')
+      .get('h1.movie-title')
+      .filter(':contains("Skylines")')
+      .first()
+      .click()
+      .get('div .App>h1').contains('Movie Details').should('be.visible')
+      .get('div .App>h3').contains('Loading').should('be.visible')
+
+    cy.location().should((localtion) => {
+      expect(localtion.pathname).to.contains('movie')
+    })
+
+    cy.wait(1000)
+    
+    cy.get('div .movie-details-title>h1').contains('Skylines').should('be.visible')
+      .get('div .movie-details-info').contains('Movie Overview').should('be.visible')
+
+    cy.scrollTo('bottom', { duration: 500 })
+    cy.scrollTo('top', { duration: 500 })
+
+    cy.get('div .movie-details-title>i.fa-chevron-left').should('be.visible').click()
+      .location().should((localtion) => {
+        expect(localtion.pathname).to.eq('/nowplaying')
+      })
   })
 
-  it('click on upcoming', () => {
+  it('upcoming', () => {
     cy.location().should((localtion) => {
       expect(localtion.pathname).to.eq('/')
     })
@@ -30,9 +57,42 @@ context('Test Movie Demo', () => {
     cy.location().should((localtion) => {
       expect(localtion.pathname).to.eq('/upcoming')
     })
+
+    cy.get('nav.navbar-wrapper')
+      .next()
+      .contains('Upcoming Movies').should('be.visible')
+      .next()
+      .contains('Loading').should('be.visible')
+
+    cy.scrollTo('bottom', { duration: 2000 })
+
+    cy.get('h1.movie-title')
+      .filter(':contains("Monster Hunter")')
+      .first()
+      .click()
+      .get('div .App>h1').contains('Movie Details').should('be.visible')
+      .get('div .App>h3').contains('Loading').should('be.visible')
+
+    cy.location().should((localtion) => {
+      expect(localtion.pathname).to.contains('movie')
+    })
+
+    cy.wait(1000)
+    
+    cy.get('div .movie-details-title>h1').contains('Monster Hunter').should('be.visible')
+      .get('div .movie-details-info').contains('Movie Overview').should('be.visible')
+
+    cy.scrollTo('bottom', { duration: 500 })
+    cy.scrollTo('top', { duration: 500 })
+
+    cy.get('div .movie-details-title>i.fa-chevron-left').should('be.visible').click()
+      .location().should((localtion) => {
+        expect(localtion.pathname).to.eq('/upcoming')
+      })
+
   })
 
-  it('click on genres', () => {
+  it('genres', () => {
     cy.location().should((localtion) => {
       expect(localtion.pathname).to.eq('/')
     })
@@ -45,9 +105,59 @@ context('Test Movie Demo', () => {
     cy.location().should((localtion) => {
       expect(localtion.pathname).to.eq('/genres')
     })
+
+    cy.scrollTo('bottom', { duration: 2000 })
+
+    cy.get('div .genre-component')
+      .find('h3')
+      .get('.genre-name')
+      .contains('Music')
+      .first()
+      .click()
+      .get('h1').first().contains('Music Movies').should('be.visible')
+    
+    cy.location().should((localtion) => {
+      expect(localtion.pathname).to.contain('/genres/Music')
+    })
+
+    cy.get('div .movie-component')
+      .first()
+      .click()
+      .get('div .App>h1').contains('Movie Details').should('be.visible')
+      .get('div .App>h3').contains('Loading').should('be.visible')
+
+    cy.location().should((localtion) => {
+      expect(localtion.pathname).to.contains('movie')
+    })
+
+    cy.wait(1000)
+    
+    cy.get('div .movie-details-title>h1').contains('Soul').should('be.visible')
+      .get('div .movie-details-info').contains('Movie Overview').should('be.visible')
+
+    cy.scrollTo('bottom', { duration: 500 })
+    cy.scrollTo('top', { duration: 500 })
+
+    cy.get('div .movie-details-title>i.fa-chevron-left')
+      .should('be.visible')
+      .click()
+      .location().should((localtion) => {
+        expect(localtion.pathname).to.contain('/genres/Music')
+      })
+    
+    cy.wait(1000)
+
+    cy.get('div .genre-search-title')
+      .find('div')
+      .get('div .fa-chevron-left')
+      .should('be.visible')
+      .click()
+      .location().should((localtion) => {
+        expect(localtion.pathname).to.contain('/genres')
+      })
   })
 
-  it('click on movie search', () => {
+  it('movie search', () => {
     cy.location().should((localtion) => {
       expect(localtion.pathname).to.eq('/')
     })
@@ -67,12 +177,40 @@ context('Test Movie Demo', () => {
       .type('Soul{enter}')
       .get('form').next()
       .contains('Searching ')
+      .should('be.visible')
 
-    cy.wait(2000)
+    // waiting for response
+    cy.wait(5000)
 
     cy.get('form').next()
       .contains('Movie Results for: ')
       .should('be.visible')
+    
+    cy.get('h1.movie-title')
+      .filter(':contains("Soul")')
+      .first()
+      .click()
+      .get('div .App>h1').contains('Movie Details').should('be.visible')
+      .get('div .App>h3').contains('Loading').should('be.visible')
+
+    cy.location().should((localtion) => {
+      expect(localtion.pathname).to.contains('movie')
+    })
+
+    cy.wait(1000)
+    
+    cy.get('div .movie-details-title>h1').contains('Soul').should('be.visible')
+      .get('div .movie-details-info').contains('Movie Overview').should('be.visible')
+
+    cy.scrollTo('bottom', { duration: 500 })
+    cy.scrollTo('top', { duration: 500 })
+
+    cy.get('div .movie-details-title>i.fa-chevron-left').should('be.visible').click()
+      .location().should((localtion) => {
+        expect(localtion.pathname).to.eq('/search')
+      })
+
+    cy.wait(1000)
     
     cy.get('input[type=submit]')
       .should('be.visible')
@@ -80,7 +218,6 @@ context('Test Movie Demo', () => {
 
     cy.get('form').next()
       .contains('Searching ')
-      //.wait(500)
       .contains('Woops, ')
       .should('be.visible')
   })
