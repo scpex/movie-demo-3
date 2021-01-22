724fefbc30f1eeef080c04fac5d97de728da9fdb
scpex <mr.nathakit@live.com>
2021-01-22 14:57:09 +0700

    resolved conflict

--- cypress/integration/examples/assertions.spec.js
+++ cypress/integration/examples/assertions.spec.js
@@ -51,7 +51,7 @@ context('Assertions', () => {
     // https://on.cypress.io/assertions
     it('expect - make an assertion about a specified subject', () => {
       // We can use Chai's BDD style assertions
-      expect(true).to.be.true
+      expect(true).to.equal(true)
       const o = { foo: 'bar' }
 
       expect(o).to.equal(o)
